6dcc9ec1f8d82a5203c7a26d4883844ca9c39473
unknown <WuzorGiftKnowledge>
2021-12-24 07:29:09 +0100

    new

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
   publicRuntimeConfig: {
       apiUrl: process.env.NODE_ENV === 'development'
           ? 'http://localhost:3000/api' // development api
-          : 'http://localhost:3000/api' // production api
+          : 'https://transaction-app.vercel.app/api' // production api
   }
  
 }

e659d4bcee7e8eeda36246f4de33881b657d41e7
unknown <WuzorGiftKnowledge>
2021-12-24 09:28:03 +0100

    new

--- pages/index.js
+++ pages/index.js
@@ -9,15 +9,17 @@ function Home() {
             <div className="container">
                 <h1>Hi {userService.userValue?.firstname}!</h1>
                 <p>You&apos;re Welcome</p>
-                <p><Link href={`/users/${userService.userValue?.id}`}>Manage Users</Link></p>
+                </div>
+                <div className="container">
+                <h6><Link href={`/users/${userService.userValue?.id}`}><a>View Transactions</a></Link></h6>
             </div>
         
         
          <div className="container">
              <h5> Account Balance  </h5>
              <p> USD: <span> {userService.userValue?.usd_balance} </span> </p>
-             <p> NGR: <span> ${userService.userValue?.ngr_balance}</span> </p>
-             <p> EUR: <span> ${userService.userValue?.eur_balance}</span> </p>
+             <p> NGR: <span> {userService.userValue?.ngr_balance}</span> </p>
+             <p> EUR: <span> {userService.userValue?.eur_balance}</span> </p>
              </div>
      </div>
     );
